066fec8d411f347a66c1cc31a725ba4677c2ff31
Mouli Kalakota <mouli.kalakota@gmail.com>
2016-05-10 23:20:02 +0530

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ivdbApp
+ * @description
+ * # ivdbApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('ivdbApp', [
+        'ngMaterial'
+    ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ivdbApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ivdbApp
+ */
+angular.module('ivdbApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
